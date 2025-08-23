61c3de52144da447d345f8213b15ec8ceffb4907
ItsabhijeetWadichar12 <wabhijeet378@gmail.com>
2025-08-23 20:49:03 +0530

    project set

---
+++ ads-generator/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h2 className='text-2xl text-red-400'>Hello!</h2>
+      <Button className="ml-5">Save</Button>
+    </div>
+  )
+}
+
+export default page
